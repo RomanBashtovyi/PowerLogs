7170eb5b69306f4f33defc86d62c2b4dec7da205
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-09-22 12:51:54 +0300

    chore: update Next.js configuration to include experimental output file tracing and clean up Vercel configuration

--- next.config.js
+++ next.config.js
@@ -8,7 +8,11 @@ const nextConfig = {
       },
     ],
   },
-  output: 'standalone',
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/**/*'],
+    },
+  },
 }
 
 module.exports = nextConfig
